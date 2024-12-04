7a7c6b4f1d5d376d248a1a99278323cf92336c2c
mike <mikemail@ algo.com>
2024-12-04 11:18:59 +0100

    names changed

---
+++ blueprint.py
@@ -0,0 +1,26 @@
+# Register this blueprint by adding the following line of code 
+# to your entry point file.  
+# app.register_functions(blueprint) 
+# 
+# Please refer to https://aka.ms/azure-functions-python-blueprints
+
+import azure.functions as func
+import logging
+
+blueprint = func.Blueprint()
+
+
+@blueprint.timer_trigger(schedule="/5 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
+
+
+@blueprint.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="myeventhub",
+                               connection="EVENT_HUB_CONNECTION_STRING") 
+def eventhub_trigger(azeventhub: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s',
+                azeventhub.get_body().decode('utf-8'))
